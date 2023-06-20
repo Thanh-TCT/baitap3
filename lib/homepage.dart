f8c0caa69e3c286be353cd5ee91c389dddf7c1b4
Thanh-TCT <trancongthanh032000@gmail.com>
2023-06-20 16:29:42 +0700

    update baitap3

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,6 +12,11 @@ class homepage extends StatefulWidget {
   State<homepage> createState() => _homepageState();
 }
 
+final usernameController = TextEditingController();
+final passwordController = TextEditingController();
+String username = "username";
+String password = "password";
+
 class _homepageState extends State<homepage> {
   Future<int?> postData() async {
     var response =
@@ -27,146 +32,170 @@ class _homepageState extends State<homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 208, 229, 240),
-      body: Column(
-        children: [
-          Divider(
-            height: 200,
-            thickness: 100,
-            color: Color.fromARGB(255, 208, 229, 240),
-            indent: 200,
-          ),
-          Container(
-            child: Text(
-              ' Welcome back',
-              style: TextStyle(color: Colors.black, fontSize: 50),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Divider(
+              height: 200,
+              thickness: 100,
+              color: Color.fromARGB(255, 208, 229, 240),
+              indent: 200,
             ),
-          ),
-          Container(
-            child: Text('Login to your account',
-                style: TextStyle(color: Colors.black, fontSize: 15)),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          SizedBox(
-            width: 350,
-            height: 60,
-            child: Container(
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.all(Radius.circular(15.0))),
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Icon(Icons.person),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Username',
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ],
+            Container(
+              child: Text(
+                ' Welcome back',
+                style: TextStyle(color: Colors.black, fontSize: 50),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            width: 350,
-            height: 60,
-            child: Container(
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.all(Radius.circular(15.0))),
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Icon(Icons.password),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Password',
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                  Divider(
-                    height: 50,
-                    thickness: 50,
-                    indent: 150,
-                  ),
-                  Container(
-                    child: Icon(
-                      Icons.remove_red_eye,
-                    ),
-                  ),
-                ],
-              ),
+            Container(
+              child: Text('Login to your account',
+                  style: TextStyle(color: Colors.black, fontSize: 15)),
             ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          SizedBox(
-            height: 50,
-            width: 350,
-            child: Container(
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: ElevatedButton(
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: 350,
+              height: 80,
+              child: SingleChildScrollView(
                 child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(Icons.person),
+                      ),
+                      SizedBox(
+                        width: 200,
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          child: TextField(
+                            controller: usernameController,
+                            decoration: InputDecoration(
+                                labelText: "Username",
+                                labelStyle: TextStyle(
+                                    color: Colors.black, fontSize: 18)),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                onPressed: () async {
-                  final statusC = await postData();
-
-                  print(statusC);
-                  if (statusC == 200) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const loginpage()));
-                  } else {
-                    print("Fail");
-                  }
-                },
               ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: [
-              Container(
-                width: 80,
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              width: 350,
+              height: 80,
+              child: SingleChildScrollView(
+                child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(Icons.password),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          child: TextField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                                labelText: "Password",
+                                labelStyle: TextStyle(
+                                    color: Colors.black, fontSize: 18)),
+                          ),
+                        ),
+                      ),
+                      Divider(
+                        height: 50,
+                        thickness: 50,
+                        indent: 150,
+                      ),
+                      Container(
+                        child: Icon(
+                          Icons.remove_red_eye,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              Container(
-                child: Text('Dont have an account ? ',
-                    style: TextStyle(color: Colors.black, fontSize: 15)),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: 50,
+              width: 350,
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(30.0)),
+                child: ElevatedButton(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                  onPressed: () async {
+                    final statusC = await postData();
+                    final username = "thanhdeptrai";
+                    final password = "123456";
+                    print(statusC);
+
+                    if (username == usernameController.text &&
+                        password == passwordController.text &&
+                        statusC == 200) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const loginpage()));
+                      setState(() {});
+                    } else {
+                      print("Username or password wrong");
+                      setState(() {});
+                    }
+                  },
+                ),
               ),
-              SizedBox(
-                width: 10,
-              ),
-              Container(
-                child: Text(' Signup',
-                    style: TextStyle(color: Colors.blue, fontSize: 15)),
-              ),
-            ],
-          ),
-        ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 80,
+                ),
+                Container(
+                  child: Text('Dont have an account ? ',
+                      style: TextStyle(color: Colors.black, fontSize: 15)),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  child: Text(' Signup',
+                      style: TextStyle(color: Colors.blue, fontSize: 15)),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
